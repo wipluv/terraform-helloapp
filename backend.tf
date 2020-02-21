653b2d45e74fa3fcb03619bcb1c4d11660aea313
wipluv <60206056+wipluv@users.noreply.github.com>
2020-02-20 17:32:49 -0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     key    = "terraform/dev/helloapp"
     region = "us-east-1"
     shared_credentials_file = "/home/intelycorellc/.aws/credentials"
-    profile = "wipluu"
+    profile = "wipluuu"
   }
 }
