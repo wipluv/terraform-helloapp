90260ebb2a1d22f4cadbb586b319de04888fdf73
wipluv <60206056+wipluv@users.noreply.github.com>
2020-02-20 17:49:07 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-ource "aws_launch_configuration" "default" {
+resource "aws_launch_configuration" "default" {
   name          = "terraform-springboot-helloapp"
   image_id      = "ami-04ff587bc1c1d65e6"
   instance_type = "t2.micro"
