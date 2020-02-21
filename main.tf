20ee7134febd3886164589b8ab32be78fcfa4e2a
wipluv <60206056+wipluv@users.noreply.github.com>
2020-02-20 17:38:38 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ ource "aws_launch_configuration" "default" {
   name          = "terraform-springboot-helloapp"
   image_id      = "ami-04ff587bc1c1d65e6"
   instance_type = "t2.micro"
-  key_name = "terraform-springboot-helloapp-"
+  key_name = "terraform-springboot-helloapp"
   security_groups = ["${aws_security_group.default.id}"]
   iam_instance_profile = "${aws_iam_instance_profile.default.name}"
   user_data = "${data.template_file.default.rendered}"
