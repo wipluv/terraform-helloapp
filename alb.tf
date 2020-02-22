d239714b85f8fc7c1ebb67c3f2616058b886ae27
wipluv <biplabsharma11@gmail.com>
2020-02-21 19:21:02 -0500

    added new files

--- alb.tf
+++ alb.tf
@@ -9,3 +9,19 @@ resource "aws_lb" "default" {
     Environment = "dev"
   }
 }
+resource "aws_lb_listener" "default" {
+  load_balancer_arn = "${aws_lb.default.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.default.arn}"
+  }
+}
+
+resource "aws_lb_target_group" "default" {
+  name     = "terraform-springboot-helloapp"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = "vpc-ffb59c85"
+}
