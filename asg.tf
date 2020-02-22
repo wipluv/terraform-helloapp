705fa10eaff81b35b208631c5a2ee32920153839
wipluv <biplabsharma11@gmail.com>
2020-02-21 19:27:44 -0500

    added new files

--- asg.tf
+++ asg.tf
@@ -8,4 +8,5 @@ resource "aws_autoscaling_group" "default" {
   force_delete              = true
   launch_configuration      = "${aws_launch_configuration.default.name}"
   vpc_zone_identifier       = ["subnet-fb8428b6", "subnet-bd223be1"]
+  target_group_arns         = ["${aws_lb_target_group.default.arn}"]
 }
